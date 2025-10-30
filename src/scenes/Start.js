80607692a00077728a2081d8688d06f4664e52b3
Calvin Richards <calvinrrichards64@gmail.com>
2025-10-30 09:37:19 -0700

    syncing code

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -10,10 +10,27 @@ export class Start extends Phaser.Scene {
         this.load.image('background', 'assets/bg.png'); // loading in assets used by the game 
         // this.load.spritesheet("monster", "filepath", {"framewidth": 64}); // supposedly how you add a spritesheet
         this.load.image('ship_1', "assets/spaceShips_004.png");
+        this.load.image('ship_2', "assets/spaceShips_002.png");
+        this.load.image('ship_3', "assets/spaceShips_003.png");
         // load ship 1, 2, 3
-        this.load.image('projectile', "assets/spaceRocketParts_002.png");
+        this.load.image('bullet', "assets/spaceRocketParts_002.png");
+        this.load.image("rocket", "assets/spaceRocketParts_001.png")
         // bullets and rockets  
-        this.load.image("planet", "assets/spaceShips_002.png");
+        this.load.image("planet0", "assets/planet00.png");
+        this.load.image("planet1", "assets/planet01.png");
+        this.load.image("planet2", "assets/planet02.png");
+        this.load.image("planet3", "assets/planet03.png");
+        this.load.image("planet4", "assets/planet04.png");
+        this.load.image("planet5", "assets/planet05.png");
+        this.load.image("planet6", "assets/planet06.png");
+        this.load.image("planet7", "assets/planet07.png");
+        this.load.image("planet8", "assets/planet08.png");
+        this.load.image("planet9", "assets/planet09.png");
+
+        this.load.image("star1", "assets/star_large.png");
+        this.load.image("star2", "assets/star_medium.png");
+        this.load.image("star3", "assets/star_small.png");
+        this.load.image("star4", "assets/star_tiny.png");
     }
 
     create() {
